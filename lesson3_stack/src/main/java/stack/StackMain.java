7628051c9eb67f932de6f65de3a094843c8875f8
Artsrun <nalbandian.a@gmail.com>
2019-02-19 22:53:57 +0300

    реализация Stack в консольной программе

--- lesson3_stack/src/main/java/stack/StackMain.java
+++ lesson3_stack/src/main/java/stack/StackMain.java
@@ -1,4 +1,41 @@
 package stack;
 
+import queue.Queue;
+
 public class StackMain {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new StackImplementation<>(5);
+        insert(stack,2);
+        insert(stack,1);
+        insert(stack,4);
+        insert(stack,5);
+        insert(stack,3);
+        insert(stack,666);
+
+        System.out.println("Size: " +stack.size());
+        System.out.println("Peek: " +stack.peek());
+
+        System.out.println("Remove last: " +remove(stack));
+        System.out.println("Size: "+stack.size());
+        System.out.println("Peek: "+stack.peek());
+
+//        while (!stack.isEmpty()){
+//            System.out.println(remove(stack));
+//        }
+    }
+
+    private static <E> E remove(Stack<E> stack) {
+        if (!stack.isEmpty()){
+            return stack.pop();
+        }
+        return null;
+    }
+
+    private static <E> void insert(Stack<E> stack, E value) {
+        if (!stack.isFull()){
+            stack.push(value);
+        }
+    }
+
 }
