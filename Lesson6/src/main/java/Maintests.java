dd4cad3eacdb89cdddd546769b034e2835249532
Artsrun <nalbandian.a@gmail.com>
2019-03-09 02:12:33 +0300

    Lesson6 hw

--- Lesson6/src/main/java/Maintests.java
+++ Lesson6/src/main/java/Maintests.java
@@ -1,23 +1,38 @@
+import java.util.Random;
+
 public class Maintests {
 
     public static void main(String[] args) {
-        Tree<Integer> tree = new TreeImpl<>();
-        tree.add(60);
-        tree.add(25);
-        tree.add(66);
-        tree.add(15);
-        tree.add(5);
-        tree.add(20);
-        tree.add(45);
-        tree.add(30);
-        tree.add(32);
-        tree.add(55);
-        tree.display();
-        tree.delete(15);
-        tree.display();
+        double notBalanced=0;
+        double balanced = 0;
+        double percentageOfBalanced;
+
+        for (int j = 0; j < 100; j++) {
+            Tree<Integer> tree = new TreeImpl<>(4);
+            for (int i = 0; i < Math.pow(2,((TreeImpl<Integer>) tree).MAX_DEPTH)-1; i++) {
+                tree.add(new Random().nextInt(200)-100);
+            }
+            //tree.display();
+            if(isBalanced(((TreeImpl<Integer>) tree).rootNode)){
+               balanced++;
+            }else notBalanced++;
+
+        }
+        percentageOfBalanced=(balanced/(balanced+notBalanced))*100;
+        System.out.println(percentageOfBalanced);
 
 
 
+    }
+
+    public static boolean isBalanced(Node node) {
+        return (node == null) ||
+                isBalanced(node.getLeftChild()) &&
+                        isBalanced(node.getRightChild()) &&
+                        Math.abs(height(node.getLeftChild()) - height(node.getRightChild())) <= 1;
+    }
+    private static int height(Node node) {
+        return node == null ? 0 : 1 + Math.max(height(node.getLeftChild()), height(node.getRightChild()));
     }
 
 }
